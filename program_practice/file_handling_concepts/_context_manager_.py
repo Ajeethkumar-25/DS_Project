f121b62c14f5ac5c30336b031e3514421ad5be06
Ajeethkumar-25 <117643912+Ajeethkumar-25@users.noreply.github.com>
2024-02-15 08:40:45 +0530

    Python Practice

---
+++ program_practice/file_handling_concepts/_context_manager_.py
@@ -0,0 +1,8 @@
+# Important Topic
+# Context Manager
+
+# file= open("file_name", "mode")
+with open("Ajeeth.txt","w") as file:
+    file.write("Hello")
+
+print(file.closed)
--- program_practice/file_handling_concepts/os_level_functions.py
+++ program_practice/file_handling_concepts/os_level_functions.py
@@ -1,10 +1,25 @@
 import os
 
-# #deletes the file
+# #deletes the file:
 # os.remove("trail.txt")
-# checks if given file addressed or not
-is_exist = os.path.exists("practice.txt")
-print(is_exist)
+# # checks if given file addressed or not
+# is_exist = os.path.exists("practice.txt")
+# print(is_exist)
+
+# # deletes the folder to use this:::Folder needs to be empty
+# os.rmdir("D:\\DS Projects\\DS_practice\\program_practice\\file_handling_concepts\\test")
+# #
+
+# # creating a file :
+# file= open("D:\\DS Projects\\DS_practice\\program_practice\\filehandling\\test1.txt", "w")
 
 
-os.rmdir("D:\\DS Projects\\DS_practice\\program_practice\\file_handling_concepts\\test")
+# x- mode : It create the file, if the file already exist then it will throw an error
+file_name = "ajeeth.txt"
+if os.path.exists("file_name")==False:
+    file =open("file_name","x")
+    print("File got created")
+else:
+    print("File already exist")
+
+
